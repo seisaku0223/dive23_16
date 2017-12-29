e923eae0cefa769f838af84e3b7392a559301ec8
peachkichi <peachkichi@peachkichinoMacBook-Pro.local>
2017-12-29 18:51:26 +0900

    db:seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,22 +6,23 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-# 10.times do |n|
-#   User.create!(name: Faker::Vehicle.vin,
-#                email: Faker::Internet.email,
-#                password: "199392199392",
-#               )
-# end
-#
-# 10.times do |note|
-#   Question.create!( title: Faker::Vehicle.vin,
-#                     content: Faker::Vehicle.manufacture,
-#                     user_id: User.last.id,
-#                     )
-# end
-
-require "csv"
-
-CSV.read('/stackoverflow_tags.csv').each do |row|
-  ActsAsTaggableOn::Tag.create(:name => row[0], :description => row[1])
+10.times do |n|
+  User.create!(name: Faker::Vehicle.vin,
+               email: Faker::Internet.email,
+               password: "199392199392",
+              )
 end
+
+10.times do |note|
+  Question.create!( title: Faker::Vehicle.vin,
+                    content: Faker::Vehicle.manufacture,
+                    user_id: User.last.id,
+                    )
+end
+
+##stackoverflow_tags.csvをdbにインポート
+# require "csv"
+#
+# CSV.read('/stackoverflow_tags.csv').each do |row|
+#   ActsAsTaggableOn::Tag.create(:name => row[0], :description => row[1])
+# end
