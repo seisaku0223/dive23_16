df783da479d2a7cf1b6b5dd3a8abf8ad7b5266b4
peachkichi <peachkichi@peachkichinoMacBook-Pro.local>
2017-12-30 12:41:47 +0900

    develop tag function in views/questions/index.html.erb and etc,generate /controllers/lists/tag_controller

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :users, only: [:index, :show]
+
+  namespace :products do
+   resources :tags, param: :tag_name, only: [:index, :show]
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
