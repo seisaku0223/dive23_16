7a895ca0a218b9f6277bce339bc0847d1d09d652
ryota <siroisagani@gmail.com>
2018-01-02 13:09:52 +0900

    votesコントローラーへcreateとdestroyアクションを追加

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,7 +1,11 @@
 class VotesController < ApplicationController
   def create
+    vote = current_user.votes.create(question_id: params[:question_id])
+    redirect_to questions_url, notice: "#{vote.question.user.name}さんの質問に投票しました"
   end
 
   def destroy
+    vote = current_user.votes.find_by(question_id: params[:question_id]).destroy
+    redirect_to questions_url, notice: "#{vote.question.user.name}さんへの投票を取り消しました"
   end
 end
