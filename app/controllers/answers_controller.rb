047992f41959d40f10fa140a3ae729fee49bc729
mem <mem0112@hotmail.com>
2018-01-07 01:37:52 +0900

    Implement delete answer

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -12,6 +12,27 @@ class AnswersController < ApplicationController
     end
   end
 
+  def destroy
+    @answer = Answer.find(params[:id])
+    @question = @answer.question
+    @answer.destroy
+    respond_to do |format|
+      if @answer.save
+        format.html { redirect_to question_path(@question) }
+        format.js{ render :index }
+      else
+        format.html{ render :new }
+      end
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+
   private
     def answer_params
       params.require(:answer).permit(:question_id, :content)
