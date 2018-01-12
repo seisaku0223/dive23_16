148e8de865751b7d0455791de6076b8024497c8f
seisaku0223 <kdoll135npc@outlook.jp>
2018-01-12 11:57:57 +0000

    Fixed favorite counter, change html question.favorites.count only

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -5,7 +5,6 @@ class FavoritesController < ApplicationController
   def create
     @question = Question.find(params[:favorite][:question_id])
     current_user.favorite!(@question)
-    @favorite_count = Favorite.where(question_id: params[:favorite][:question_id]).count
     respond_with @question
   end
 
@@ -13,7 +12,6 @@ class FavoritesController < ApplicationController
     @question = Favorite.find(params[:id]).question
     count_id = @question.id
     current_user.unfavorite!(@question)
-    @favorite_count = Favorite.where(question_id: count_id).count
     respond_with @question
   end
 end
