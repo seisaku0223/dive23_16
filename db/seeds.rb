a26d4392da0174af99e9501a85073998ce290f39
seisaku0223 <kdoll135npc@outlook.jp>
2018-01-12 14:20:11 +0000

    Add seed date, User 50, user_id 1 date Question Favorite Vote Answer 44 date, and random 2-12 user_id 10 date create

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,19 +6,59 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-10.times do |n|
-  User.create!(name: Faker::Vehicle.vin,
+50.times do |n|
+  User.create!(name: Faker::Name.name,
                email: Faker::Internet.email,
                password: "199392199392",
               )
 end
 
-30.times do |note|
+60.times do |note|
+  user_id =  rand(2..20)
   Question.create!( title: Faker::Vehicle.vin,
                     content: Faker::Vehicle.manufacture,
-                    user_id: User.last.id,
-                    )
+                    user_id: user_id,
+                  )
 end
+
+1.times do |o|
+  user_id =  1
+  44.times do |m|
+  question_id = o + m + 1
+  Question.create!( title: Faker::Vehicle.vin,
+                    content: Faker::Vehicle.manufacture,
+                    user_id: user_id,
+                  )
+  Favorite.create!( user_id: user_id,
+                    question_id: question_id,
+                  )
+  Vote.create!( user_id: user_id,
+                question_id: question_id,
+              )
+  Answer.create!( user_id: user_id,
+                  question_id: question_id,
+                  content: Faker::Lorem.sentence,
+                  )
+  end
+end
+
+10.times do |h|
+  user_id =  h + 2
+  10.times do |i|
+  question_id = h + i + 1
+  Favorite.create!( user_id: user_id,
+                    question_id: question_id,
+                  )
+  Vote.create!( user_id: user_id,
+                question_id: question_id,
+              )
+  Answer.create!( user_id: user_id,
+                  question_id: question_id,
+                  content: Faker::Lorem.sentence,
+                  )
+  end
+end
+
 
 ##stackoverflow_tags.csvをdbにインポート
 # require "csv"
