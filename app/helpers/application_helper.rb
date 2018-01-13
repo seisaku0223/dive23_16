ac5b777ac91d111ab8f50e275d4881d9f515a68c
seisaku0223 <kdoll135npc@outlook.jp>
2018-01-13 06:46:03 +0000

    Fixed total point to index.html.erb and show.html.erb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,20 @@ module ApplicationHelper
       img_url = 'no_image.png'
     image_tag(img_url, alt: user.name)
   end
+
+  def contribution_vote_count(user)
+    @count_vote = 0
+    user.questions.each do |data|
+      @count_vote += data.vote_users.count
+    end
+    return @count_vote
+  end
+
+  def contribution_answer_count(user)
+    @count_answer = 0
+    user.questions.each do |data|
+      @count_answer += data.answers.count
+    end
+    return @count_answer
+  end
 end
